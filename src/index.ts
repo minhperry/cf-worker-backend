f6ae62fed3cc14af4e7ef6b37b7c922b6b26c4c3
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-08-26 16:46:24 +0200

    feat: github commit backend

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import { socialDataHandler } from './data/social-data'
 import { skyblockXpHandler } from './skyblock/skyblock'
 import { verifyHash } from './login/md5'
 import { Shorts } from './login/short'
+import { getGithubCommits } from './data/github'
 
 type Binding =  {
     HYPIXEL: string    
     PASSWORD: string
     MYKEY: string
     shortener: KVNamespace
+    GITHUB: string
 }
 
 const app = new Hono<{Bindings: Binding}>()
@@ -18,18 +20,16 @@ app.use(
     cors()
 )
 
-
-
 app
     .get('/', (c) => c.html('<p>It worked!</p>'))
     .get('/socials', socialDataHandler)
     .get('/skyblock/xp/:name', skyblockXpHandler)
     .get('/info', (c) => c.json({ source: 'https://github.com/minhperry/cf-worker-backend' }))
     .get('/validate/:md5', (c) => verifyHash(c))
+    .get('/short', async (c) => new Shorts(c).list())
+    .get('/commits', async (c) => getGithubCommits(c, 50))
 
 app.post('/short', async (c) => new Shorts(c).create())
-
-app.get('/short', async (c) => new Shorts(c).list())
 
 export default app
 
